b46ce16fdca4fa7429fdcacaf3b3545e8e967553
Melissa Redd <melissa.redd@amgcorp.com>
2013-12-19 15:42:57 -0800

    Plugin Update
    Plugin Update

--- event_reminder.php
+++ event_reminder.php
@@ -1,154 +1,138 @@
 <?php
-/*
+
+/***********
+
 		Plugin Name: AMG Event Reminder
+
 		Plugin URI: http://www.angelmg.com
-		Description: Plugin to remind vip guestlist registrants of event date.
+
+		Description: Reminds users of their event registration.
+
 		Author: Melissa Redd
-		Version: 1.0
-		Release Date: December 12th, 2013
+
+		Version: 2.0
+
+		Release Date: December 18th, 2013
+
 		Author URI: http://www.angelmg.com
-		*/
-		
-// Declare custom time intervals for email reminders
-add_filter( 'cron_schedules', 'cron_add_intervals' );
-function cron_add_intervals($schedules) {
-    $schedules['twodays'] = array(
- 		'interval' => 172800,
- 		'display' => __('Once Every Two Days')
- 	);
- 	$schedules['threedays'] = array(
- 		'interval' => 259200,
- 		'display' => __('Once Every Three Days')
- 	);
-    $schedules['weekly'] = array(
- 		'interval' => 604800,
- 		'display' => __('Once Weekly')
- 	);
- 	$schedules['biweekly'] = array(
- 	    'interval' => 1209600,
- 	    'display' => __('Once Biweekly')
- 	);
- 	$schedules['monthly'] = array(
- 	    'interval' => 2419200,
- 	    'display' => __('Once Monthly')
- 	);
-    return $schedules;
-}
 
-// Schedule daily check upon plugin activation
-register_activation_hook( __FILE__, 'amg_event_reminder_install' );
-add_action('aer_hook', 'check_days');
-function aer_activation() {
-    $set_time = 1304431200; // 9:00AM EST
-    $file = ABSPATH . "/wp-content/plugins/amg_event_reminder/event_reminder.php";
-	if (file_exists($file)) {
-	   wp_schedule_event($set_time, 'daily', 'aer_hook');
+		***********/
+
+	
+
+function event_reminder_table()
+
+{
+
+      	global $wpdb;
+
+ 
+
+	
+
+	if($wpdb->get_var("show tables like event_reminder_table") != 'event_reminder_table') 
+
+	{
+
+		$sql = "CREATE TABLE event_reminder_table (
+
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+
+		first_name tinytext NOT NULL,
+
+		last_name tinytext NOT NULL,
+
+		phone tinytext NOT NULL,
+
+		email tinytext NOT NULL,
+
+		guest_number tinytext NOT NULL,
+
+		event_name tinytext NOT NULL,
+
+		event_date tinytext NOT NULL,
+
+		form_status tinytext NOT NULL,
+
+		form_type tinytext NOT NULL,
+
+		UNIQUE KEY id (id)
+
+		);";
+
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+		dbDelta($sql);
+
 	}
+
 }
 
-// Remove scheduled daily checks upon plugin deactivation
-register_deactivation_hook(__FILE__, 'amg_event_reminder_uninstall');
-function aer_deactivation() {
-	wp_clear_scheduled_hook('aer_hook');
-}
-		
+// this hook will cause our creation function to run when the plugin is activated
+
+register_activation_hook( __FILE__, 'event_reminder_table' );
+
+
+
+add_action("gform_after_submission", "push_fields", "test", 10, 2);
+
+
+
+function push_fields($entry, $form){
+
+
+
+if($_POST["input_7"] == 'submitted'){	
+
 global $wpdb;
 
-//let's create user registration table.
-   			$table = $wpdb->prefix."vip_registration_event_reminder";
-    		$structure = "CREATE TABLE IF NOT EXISTS `$table` (
-						  `first_name` varchar(255) default NULL,
-						  `fast_name` varchar(255) default NULL,
-						  `phone` int(20) NOT NULL default '0',
-						  `email` varchar(255) default NULL,
-						  `guest_number` int(20) NOT NULL default '0',
-						  'event_name' varchar(255) NOT NULL,
-						  'event_date' DATE,
-						  'form_status' varchar(255) NOT NULL,
-						  ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
-    		$wpdb->query($structure);
-	?>
-<?php
-
-//tells gravity forms where to send user data
-if($_POST['vip_reservation_form'] == 'yes'){
-			
-			
-			add_action("gform_after_submission", "input_fields", 10, 2);
-			function input_fields($entry, $form){
-    		global $wpdb;
 
 
-                    $entry["1"] = '$first_name';
-					  $entry["2"] = '$last_name'; 
-                    $entry["3"] = '$phone';
-                    $entry["4"] = '$email';
-                    $entry["5"] = '$guest_number';
-					  $entry["6"] = '$event_name';
-					  $entry["7"] = '$event_date';
-					  $entry["8"] = '$form_status';
-					  $entry["9"] = '$form_type';
-					  
-   $SQL = "INSERT INTO vip_registration_event_reminder ( 1, 2, 3, 4, 5, 6, 7, 8, 9) VALUES ( '$first_name', '$last_name' ,'$phone', '$email', '$guest_number', '$event_name','$event_date','form_status','form_type')";
-   $wpdb->query($SQL);
-   }
+$firstName = $entry["1"];
+
+$lastName = $entry["2"];
+
+$phone = $entry["3"];
+
+$email = $entry["4"];
+
+$guestNumber = $entry["5"];
+
+$eventName = $entry["6"];
+
+$eventDate = $entry["9"];
+
+$form_status = $entry["7"];
+
+$form_type = $entry["8"];
+
+
+
+
+
+$insertdata = "INSERT INTO event_reminder_table (first_name, last_name, phone, email, guest_number,event_name,event_date,form_status, form_type) VALUES ('$firstName','$lastName', '$phone', '$email', '$guestNumber', '$eventName', '$eventDate', '$form_status', '$form_type')";
+
+
+
+$wpdb->query($insertdata);
+
 }
-   ?>
- <?php
-   
-//Runs event reminder script.
 
+}
 
-// Check if event is approaching in two days
-function check_days() {
+function test () {
 	global $wpdb;
+		$active_rows = $wpdb->get_results(
+		"SELECT * FROM event_reminder_table WHERE event_date >= NOW() - INTERVAL 2 Day"
+	);
+
+	foreach ($active_rows as $active_row){
 	
-	$amgreminders = $pd->get_amgreminders();
-	$eventdata = $amgreminders = $wpdb->get_results( $wpdb->prepare("
-			SELECT *
-			FROM {$wpdb->vip_registration_event_reminder}
-			WHERE event_date = '{$eventdate}'
-				AND form_type = 'guestlist'
-				AND post_status = '{$status}'
-			ORDER BY event_date ASC
-		") );
-	return $amgreminders;
-	
-	foreach( $amgreminders as $amgreminder ){ {
-		if( $form_status == 'registered' ){
-			$eventreminderdate = date( 'm.d.y' , strtotime($eventdate. ' - 2 days'));
-			$currentdate = date("m.d.y");
-			if ($currentdate >= $eventreminderdate) {
-				send_ereminders;	}
-			}
-		}
+	$subject = '[Reminder] ' . $active_row->event_name;
+			$to = $active_row->email;
+			$message = "<p>This message is a reminder that you are scheduled to attend";
+			wp_mail( $to, $subject, $message);
+		
 	}
 }
 
-	function send_ereminders(){
-		
-		//get ereminders
-		$pd = new PDER;
-		$ereminders = $pd->get_ereminders();
-		
-		foreach( $amgreminders as $amgreminder ){
-		
-			$subject = '[Reminder] ' . $amgreminder->$event_name;
-			$to = $ereminder->$email;
-			$headers = 	"From: AngelMG <{$email}>\r\n" .
-						"Content-Type: text/html;\r\n";
-			
-			$creation_date = date( 'l, F j, Y', strtotime( $amgreminder->event_date ) );
-			$message = "<p>This message is a reminder that you are scheduled to attent {$event_name} on {$event_date} </p>\n";
-						
-			$email_result = wp_mail( $to, $subject, $message, $headers );
-			//$email_result = wp_mail( 'ryannmicua@gmail.com', 'Test Reminder', 'message', 'From: Email Reminder <ryannmicua@gmail.com>' );
-			
-			
-			if( $email_result ){//wp_mail() processed the request successfully
-				//set post to 'publish' or delete the post
-				
-			}
-			
-		}
-	}
